fc7814cb1cdee74d33bc5c73e42f24b536f4305d
Pamela Abreu <pamelaabreu@pursuit.com>
2019-05-15 21:16:18 -0400

    Moved the Switch with the trip routes to render inside of JSX div and imported the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import {Switch, Route, HashRouter} from 'react-router-dom'
-import Trip from './containers/Trip/Trip'
+import {Switch, Route, HashRouter} from 'react-router-dom';
+import Trip from './containers/Trip/Trip';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
     render () {
         return (
             <div className="App">
                 <HashRouter>
-                    <Switch>
-                        <Route path='/trip' exact component={Trip}></Route>
-                    </Switch>
+                    <Route path='/' component={Navbar} />
+                    <div>
+                        <Switch>
+                            <Route path='/trip' exact component={Trip}></Route>
+                        </Switch>
+                    </div>
                 </HashRouter>
             </div>
         );
