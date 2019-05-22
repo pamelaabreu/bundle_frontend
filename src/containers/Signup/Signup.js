a25576da8bc0adb2fac0cfda2a0ea731e2b94a5c
Pamela Abreu <pamelaabreu@pursuit.com>
2019-05-22 01:48:17 -0400

    Intialized Signup component

---
+++ src/containers/Signup/Signup.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Signup extends React.Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default Signup;
