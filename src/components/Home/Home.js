c861d0720f0a35714a93687c268bf98eaa442c32
Srirupa <srirupadatta@pursuit.org>
2019-05-23 16:36:49 -0400

    inserted CreateTripForm Component; adjusted wording of taglines

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,39 +1,40 @@
 import React from 'react';
 import './Home.css';
+import CreateTripForm from '../../containers/CreateTripForm/CreateTripForm';
 
 const Home = props => {
     return (
         <>
             <div className='bundleHomeImage'>
                 <h1 className='bundleHomeImageTitle'>Bundle</h1>
-                <h2 className='bundleHomeImageTagline'>Worries less...</h2>
+                <h2 className='bundleHomeImageTagline'>Worry less, travel more!</h2>
+                <CreateTripForm />
             </div>
-            
+
             <div className='bundleHomeArrow'></div>
-            
-            <div class="container bundleHomeWhatBox">
-                <div class="row">
-                    <div class="col-sm">
+
+            <div className="container bundleHomeWhatBox">
+                <div className="row">
+                    <div className="col-sm">
                         <div className='bundleHomeWhatPhotoBox'></div>
                     </div>
-                    <div class="col-sm">
+                    <div className="col-sm">
                         <h2>What's Bundle</h2>
                         <p>
-                        We provide a trip-management hub for inexperienced 
-                        travelers to keep track of all their necessities. 
-                        They’ll have a smoother and more enjoyable experience 
-                        preparing for it because they can address all their 
-                        travel considerations from one place. 
-                        Bundle creates suggested packing checklists and help 
-                        complete them with in-app planning until day of departure.
+                            We provide a trip-management hub for inexperienced
+                            travelers to keep track of all their necessities.
+                            They’ll have a smoother and more enjoyable experience
+                            preparing for it because they can address all their
+                            travel considerations from one place.
+                            Bundle creates suggested packing checklists and help
+                            complete them with in-app planning until day of departure.
                         </p>
                     </div>
                 </div>
             </div>
-            
-            <div class="container-fluid bundleHomeGetPackingBox">
-                <h1>What are you waiting for? Get Bundle!</h1>
-                <button>Placeholder for Create Trip</button>
+
+            <div className="container-fluid bundleHomeGetPackingBox">
+                <h1>What are you waiting for? Get Packing!</h1>
             </div>
         </>
     );
