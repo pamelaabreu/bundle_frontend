c0dd0416f23a0e743ad62d31f93a7b1266a7202f
Pamela Abreu <pamelaabreu@pursuit.com>
2019-05-23 00:09:27 -0400

    Reformatted file

--- src/containers/Signup/Signup.js
+++ src/containers/Signup/Signup.js
@@ -1,71 +1,73 @@
-import React from 'react';
+import React from "react";
 
-import firebase from '../../firebase';
+import firebase from "../../firebase";
 
 class Signup extends React.Component {
-    state = {
-        inputs: {
-            username: "", 
-            email: "",
-            password: ""
-        },
-        fillFormInputError: "",
-        firebaseCreateUserError: ""
-    }
+  state = {
+    inputs: {
+      username: "",
+      email: "",
+      password: ""
+    },
+    fillFormInputError: "",
+    firebaseCreateUserError: ""
+  };
 
-    handleInputChange = e => {
-        const { inputs } = {...this.state};
-        inputs[e.target.name]= e.target.value.trim();
+  handleInputChange = e => {
+    const { inputs } = { ...this.state };
+    inputs[e.target.name] = e.target.value.trim();
 
-        this.setState({ inputs });
-    }
+    this.setState({ inputs });
+  };
 
-    handleSignupButtonClick = e => {
-        e.preventDefault();
+  handleSignupButtonClick = e => {
+    e.preventDefault();
 
-        const { inputs } = this.state;
-        const { email, password } = inputs;
-            
-            
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(
-            response => response.user.uid,
-            
-            // This is the handling for the Firebase error when creating a user 
-            ({message}) => this.setState({firebaseCreateUserError: message })
-        )
-        .then(firebaseUid => {
-            // Logic for creating user in the backend
-        })
+    const { inputs } = this.state;
+    const { email, password } = inputs;
 
-    }
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(
+        response => response.user.uid,
 
-    render () {
-        const { inputs, fillFormInputError, firebaseCreateUserError } = this.state;
-        const inputsArray = Object.entries(inputs);
+        // This is the handling for the Firebase error when creating a user
+        ({ message }) => this.setState({ firebaseCreateUserError: message })
+      )
+      .then(firebaseUid => {
+        // Logic for creating user in the backend
+      });
+  };
 
-        return (
-                <form>
-                    {fillFormInputError ? <p>{fillFormInputError}</p> : null}
-                    {firebaseCreateUserError ? <p>{firebaseCreateUserError}</p> : null}
-                    {inputsArray.map(([inputName, inputValue], index) => {
-                        const inputType = inputName.toLowerCase() === 'password' ? "password" : "text";
-                        const isRequired = inputName.toLowerCase() === 'username' ? false : true;
-                        return <input 
-                                onChange={this.handleInputChange} 
-                                key={index} 
-                                type={inputType} 
-                                value={inputValue} 
-                                name={inputName} 
-                                placeholder={inputName} 
-                                required={isRequired}
-                                min='1'
-                            />
-                    })}
-                    <button onClick={this.handleSignupButtonClick}>Signup</button>
-                </form>
-        );
-    }
+  render() {
+    const { inputs, fillFormInputError, firebaseCreateUserError } = this.state;
+    const inputsArray = Object.entries(inputs);
+
+    return (
+      <form>
+        {fillFormInputError ? <p>{fillFormInputError}</p> : null}
+        {firebaseCreateUserError ? <p>{firebaseCreateUserError}</p> : null}
+        {inputsArray.map(([inputName, inputValue], index) => {
+          const inputType =
+            inputName.toLowerCase() === "password" ? "password" : "text";
+          const isRequired =
+            inputName.toLowerCase() === "username" ? false : true;
+          return (
+            <input
+              onChange={this.handleInputChange}
+              key={index}
+              type={inputType}
+              value={inputValue}
+              name={inputName}
+              placeholder={inputName}
+              required={isRequired}
+              min="1"
+            />
+          );
+        })}
+        <button onClick={this.handleSignupButtonClick}>Signup</button>
+      </form>
+    );
+  }
 }
 
 export default Signup;
