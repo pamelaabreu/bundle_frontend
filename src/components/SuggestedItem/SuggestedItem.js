c6e085595a2c79b031946ce883e60912f6d38d51
Robert Abreu <robertabreu@pursuit.org>
2019-05-25 16:58:56 -0400

    wrapped component in button, moved onClick to this element

--- src/components/SuggestedItem/SuggestedItem.js
+++ src/components/SuggestedItem/SuggestedItem.js
@@ -11,39 +11,47 @@ export default props => {
     index
   } = props;
   const toBePacked = pack ? "item--font--packed" : "item--font--unpacked";
-  const borderColor = pack ? " item--border--packed " : " item--border--unpacked ";
+  const borderColor = pack
+    ? " item--border--packed "
+    : " item--border--unpacked ";
   return (
-    <div
-      className={toBePacked + borderColor +"  item p-0 m-1 rounded"}
-      onClick={handleClick("bundle", index)}
-    >
-      <div className="container">
-        <div className={"row align-items-center justify-content-center"}>
-          <div className="col-12 px-1 pt-1  text-left div--icon--size">
-            {/* ICONS */}
-            {pack ? (
-              <i className="far fa-check-circle align-top item--icon--bundled" />
-            ) : (
-              <i className="far fa-circle align-top item--icon--unbundled" />
-            )}
+    <div className={"p-0 m-1"}>
+      <button
+        className={toBePacked + borderColor + " item rounded"}
+        onClick={handleClick("bundle", index)}
+      >
+        <div className="container">
+          <div className={"row align-items-center justify-content-center"}>
+            <div className="col-12 px-1 pt-1  text-left div--icon--size">
+              {/* ICONS */}
+              {pack ? (
+                <i className="far fa-check-circle align-top item--icon--bundled" />
+              ) : (
+                <i className="far fa-circle align-top item--icon--unbundled" />
+              )}
+            </div>
+            <div>
+              {/* ITEM IMAGE */}
+              <img
+                src={image}
+                className="item--image--size rounded"
+                alt={name}
+              />
+            </div>
           </div>
-          <div>
-            {/* ITEM IMAGE */}
-            <img src={image} className="item--image--size rounded" alt={name} />
+        </div>
+        <div className="col-12">
+          {/* Item Name */}
+          <div className={"row align-items-center justify-content-center"}>
+            <span className="item--title--font">{name}</span>
           </div>
         </div>
-      </div>
-      <div className="col-12">
-        {/* Item Name */}
-        <div className={"row align-items-center justify-content-center"}>
-          <span className="item--title--font">{name}</span>
+        <div className="col-12" onClick={handleClick("select", index)}>
+          <div className={"row align-items-center justify-content-center mt-3"}>
+            <span className="item--bundle--font">Pack</span>
+          </div>
         </div>
-      </div>
-      <div className="col-12" onClick={handleClick("select", index)}>
-        <div className={"row align-items-center justify-content-center mt-3"}>
-          <span className="item--bundle--font">Pack</span>
-        </div>
-      </div>
+      </button>
     </div>
   );
 };
