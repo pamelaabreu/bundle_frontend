3ab6019d271b727558a93293e51cd25357311a09
Pamela Abreu <pamelaabreu@pursuit.com>
2019-05-25 12:34:24 -0400

    Imported FirebaseAuthContext and organized the imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import {Switch, Route, HashRouter} from 'react-router-dom';
+
+// Pages
 import Trip from './containers/Trip/Trip';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+
+// Context
+import FirebaseAuthContext from './context/FirebaseAuth';
 
 class App extends Component {
     render () {
