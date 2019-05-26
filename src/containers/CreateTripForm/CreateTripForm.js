a7d57656f04dda0ec7de589f715d8658f0e3c539
Srirupa <srirupadatta@pursuit.org>
2019-05-25 21:49:15 -0400

    added value attributes and event handlers on input tags

--- src/containers/CreateTripForm/CreateTripForm.js
+++ src/containers/CreateTripForm/CreateTripForm.js
@@ -8,51 +8,63 @@ export default props => {
     let tomorrow = moment().add(1, "day").format("YYYY-MM-DD");
 
     return (
+        <>
+
         <div className="create-trip-form">
-            <form className="">
-                <div className="form-group row justify-content-between">
-                    <label htmlFor="destination" className="trip-form-label">
-                        Enter your destination:
-                    </label>
-                    <input 
-                        type="text" 
-                        name="destination" 
-                        placeholder="City, Country" 
-                    />
-                </div>
-                <div className="form-group row justify-content-between">
-                    <label htmlFor="departure" className="trip-form-label">
-                        Enter departure date:
-                    </label>
-                    <input
-                        type="date"
-                        id="departure"
-                        name="departure"
-                        min={today}
-                        required
-                        pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
-                    />
-                </div>
-                <div className="form-group row justify-content-between">
-                    <label htmlFor="departure" className="trip-form-label">
-                        Enter return date:
-                    </label>
-                    <input
-                        type="date"
-                        id="return"
-                        name="return"
-                        min={tomorrow}
-                        required
-                        pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
-                    />
-                </div>
-                <div className='form-button-container'>
-                    <button className='form-button' type='submit'>
-                        Next
-                    </button>
-                </div>
+            <form className="" onSubmit={props.createTripHandler}>
+            <div className="form-group row justify-content-between">
+                <label htmlFor="destination" className="trip-form-label">
+                Enter your destination:
+                </label>
+                <input
+                type="text"
+                name="destination"
+                placeholder="City, Country"
+                onChange={props.destinationHandler}
+                value={props.destination}
+                required
+                />
+            </div>
+            <div className="form-group row justify-content-between">
+                <label htmlFor="departure" className="trip-form-label">
+                Enter departure date:
+                </label>
+                <input
+                type="date"
+                id="departure"
+                name="departure"
+                min={today}
+                required
+                pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+                onChange={props.departureDateHandler}
+                value={props.departureDate}
+                />
+            </div>
+            <div className="form-group row justify-content-between">
+                <label htmlFor="departure" className="trip-form-label">
+                Enter return date:
+                </label>
+                <input
+                type="date"
+                id="return"
+                name="return"
+                min={tomorrow}
+                required
+                pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+                onChange={props.returnDateHandler}
+                value={props.returnDate}
+                />
+            </div>
+            <div className="form-button-container">
+                <button
+                className="form-button"
+                type="submit"
+                >
+                Next
+                </button>
+            </div>
             </form>
         </div>
+        </>
     );
 };
-
