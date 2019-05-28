689f6ab614b6b62fad902136004191d324db9f85
Pamela Abreu <pamelaabreu@pursuit.com>
2019-05-27 22:13:46 -0400

    Formatted component with Prettier

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,33 @@
-import React, {useContext} from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
-import FirebaseAuthContext from '../../context/FirebaseAuth';
+import FirebaseAuthContext from "../../context/FirebaseAuth";
 
 const Navbar = props => {
-    const FirebaseUserAuth = useContext(FirebaseAuthContext);
+  const FirebaseUserAuth = useContext(FirebaseAuthContext);
 
-    const UserLogout = () => {
-        FirebaseUserAuth.logoutUser();
-        props.history.push("/");
-    };
+  const UserLogout = () => {
+    FirebaseUserAuth.logoutUser();
+    props.history.push("/");
+  };
 
-    return (
-        <div className='bundleNavbar'>
-            <Link to='/'><div className='bundleNavbarLogo'>Logo</div></Link>
-            <Link to='/'>Home</Link>
-            <Link to='/login'>Login</Link>
-            <Link to='/signup'>Sign Up</Link>
-            <Link to='packHacks'>Pack Hacks</Link>
-            <Link to='/tsaGuides'>TSA Guides</Link>
-            <Link to='/about'>About</Link>
-            {!FirebaseUserAuth.user ? null : <button onClick={UserLogout}>Logout</button>}
-        </div>
-    );
+  return (
+    <div className="bundleNavbar">
+      <Link to="/">
+        <div className="bundleNavbarLogo">Logo</div>
+      </Link>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Sign Up</Link>
+      <Link to="packHacks">Pack Hacks</Link>
+      <Link to="/tsaGuides">TSA Guides</Link>
+      <Link to="/about">About</Link>
+      {!FirebaseUserAuth.user ? null : (
+        <button onClick={UserLogout}>Logout</button>
+      )}
+    </div>
+  );
 };
 
 export default Navbar;
