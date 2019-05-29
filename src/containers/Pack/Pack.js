07c4c4e69c1cc1725cadd712e99fbd6bb46719a9
Robert Abreu <robertabreu@pursuit.org>
2019-05-29 17:46:34 -0400

    made minimum required calls to back end to fetch bag info

--- src/containers/Pack/Pack.js
+++ src/containers/Pack/Pack.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import axios from 'axios';
-import UnpackedItem from "../../components/UnpackedItem";
+import axios from "axios";
+import BASEURL from "../../services/backendUrlConnect";
+import UnpackedItem from "../../components/UnpackedItem/UnpackedItem";
 import "./Pack.css";
 
 const bag = (items = []) => {
@@ -10,7 +11,7 @@ const bag = (items = []) => {
         {items.map((e, i) => {
           return (
             <div className="col-6 col-sm-6 col-md-4 p-0" key={i}>
-              <UnpackedItem />
+              {/* <UnpackedItem /> */}
             </div>
           );
         })}
@@ -23,18 +24,35 @@ export default (class Pack extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      tripInfo: null,
       page: null,
       bags: null,
       currentBag: null,
       currentCategory: null,
       lists: null
     };
-  }
+  };
 
   async componentDidMount() {
     const { trip_id } = this.props.match.params;
-
-  }
+    const tripBagsAndLists = axios({
+      method: "get",
+      url: BASEURL + "/trip/init/" + trip_id
+    });
+    const allCategories = axios({
+      method: "get",
+      url: BASEURL + "/categories/all"
+    });
+    try {
+      const [tripDetails, categories] = await Promise.all([
+        tripBagsAndLists,
+        allCategories
+      ]);
+      console.log(tripDetails, categories)
+    } catch (err) {
+      console.log("ERROR: ", err);
+    };
+  };
 
   componentWillUnmount() {}
 
@@ -44,7 +62,7 @@ export default (class Pack extends Component {
     const { page } = this.state;
     switch (page) {
       case "bags":
-          return b
+        return;
     }
     return <></>;
   }
