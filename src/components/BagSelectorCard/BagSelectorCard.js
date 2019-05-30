520dbf7c265c88748c5abd2ca3bb4fb46db28ac4
Robert Abreu <robertabreu@pursuit.org>
2019-05-29 23:19:53 -0400

    added onClickHandler function; changed div tag to button to attach the onClick to it

--- src/components/BagSelectorCard/BagSelectorCard.js
+++ src/components/BagSelectorCard/BagSelectorCard.js
@@ -2,17 +2,15 @@ import React from "react";
 
 
 export default (props => {
-    const {bag_type, item_count} = props;
+    const {bag_id, bag_type, item_count, handleOnClick} = props;
     return (
-        <div className="row">
-          <div className="card pack--bag-width">
-            <div className="card-body">
+          <div className="col-3 mx-1 mt-2 card p-0 pack--bag-width">
+            <button className="card-body row" onClick={handleOnClick("bag", bag_id)}>
               <h5 className="card-title">{bag_type}</h5>
               <p className="card-text"> 
                 <span>{item_count} Items unpacked</span>
               </p>
-            </div>
-          </div>
+            </button>
         </div>
     );
 });
