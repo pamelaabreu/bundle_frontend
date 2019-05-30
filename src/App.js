360ee9b573246416a734e0abb90a477f986fe462
Srirupa <srirupadatta@pursuit.org>
2019-05-29 22:44:36 -0400

    imported Reminders Component and inserted it as a Route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route, HashRouter } from "react-router-dom";
 import Trip from "./containers/Trip/Trip";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import Reminders from "./containers/Reminders/Reminders";
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
             <Switch>
               <Route path="/trip/:trip_id" exact component={Trip} />
               <Route path="/" exact component={Home} />
+              <Route path="/reminders" exact component={Reminders} />
             </Switch>
           </div>
         </div>
