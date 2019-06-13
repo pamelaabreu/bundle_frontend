df7046d11ec2fa55e9799f53386ec4657e576568
Pamela Abreu <43766342+pamelaabreu@users.noreply.github.com>
2019-06-13 17:59:19 -0400

    Toast Message Style Update (#76)
    * Changed the colors for the fancy-progress-bar class style
    
    * Added a color variable to toastDropShadow class style
    
    * Created toast-text-color class style
    
    * Changed the classnames and toast preferences

--- src/components/ToastNotif/ToastNotif.js
+++ src/components/ToastNotif/ToastNotif.js
@@ -5,14 +5,14 @@ import "./ToastNotif.css";
 
 const ToastNotif = (image, text) => {
   const ToastNotifContent = (
-    <div className="mb-3">
+    <div className=" p-0 m-0">
       <div className="row no-gutters">
-        <div className="col-4">
+        <div className="col-2 p-0">
           <img src={image} className="card-img" alt={text} />
         </div>
-        <div className="col-md-8">
+        <div className="col-10">
           <div className="card-body">
-            <h5 className="card-title">{text}</h5>
+            <h5 className="card-title h4 toast-text-color">{text}</h5>
           </div>
         </div>
       </div>
@@ -20,10 +20,11 @@ const ToastNotif = (image, text) => {
   );
 
   return toast(ToastNotifContent, {
-    autoClose: 4000,
+    autoClose: 3000,
     transition: Bounce,
     progressClassName: "fancy-progress-bar",
-    className: "toastDropShadow"
+    className: "toastDropShadow",
+    pauseOnHover: false
   });
 };
 
