d748c0c1287451314a07341422c953f2f2442a9a
Pamela Abreu <pamelaabreu@pursuit.com>
2019-06-13 15:53:38 -0400

    Changed the JSX semantics and classnames for publicHome

--- src/components/PublicHome/PublicHome.js
+++ src/components/PublicHome/PublicHome.js
@@ -1,30 +1,35 @@
 import React from "react";
 import "./PublicHome.css";
 import BundleLogo from "../../assets/images/logo/bundle_logo.svg";
+// import BundleLoad from "../../assets/images/background_images/bundle_load.gif";
 import LoadingScreen from "../LoadingScreen/LoadingScreen";
 
 const PublicHome = props => {
   const { create_trip_form, loading } = props;
 
   return (
-    <>
+    <div className="publicHomeBanner m-0 p-0 min-vh-100 min-vw-100 d-flex justify-content-center align-items-center">
+      {/* <div className="bg-bundleBlue m-0 p-0 min-vh-100 min-vw-100 d-flex justify-content-center align-items-center">   */}
       {!loading ? null : <LoadingScreen />}
-      <div className="publicHomeBanner container-fluid mb-5 min-vh-100">
-        <div className="row p-5 h-75">
-          <div className="col-sm m-5 pt-0 px-5 pb-2">
-            <img src={BundleLogo} width="150" height="150" alt="Bundle" />
-            <h2 className="c-white mali700 mt-5 h1">
-              Worry less, travel more!
-            </h2>
-          </div>
 
-          <div className="col-sm m-5 p-5 bg-huate80 b-radius9 pb-5">
-            <h2 className="c-bundleBlue mali700 mb-5 h1">Let's get packing!</h2>
-            {create_trip_form}
-          </div>
+      <div className="row p-5 m-0 publicHome-width">
+        <div className="col-sm-6 col-md-6 col-lg-6 text-center">
+          <img
+            src={BundleLogo}
+            width="70%"
+            height="70%"
+            className=""
+            alt="Bundle"
+          />
+          {/* <img src={BundleLoad} width="100%" height="100%" className="img-thumbnail border-0 p-0 bg-transparent" alt="Bundle" /> */}
+        </div>
+
+        <div className="col-sm-6 col-md-6 col-lg-6 p-5 bg-denimBlue b-radius9 ds-lightGrey createTripForm-mb">
+          <h2 className="c-huate mali700 mb-5 display-3">Let's get packing!</h2>
+          {create_trip_form}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
