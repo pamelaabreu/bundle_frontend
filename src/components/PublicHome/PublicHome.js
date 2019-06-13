b43e6eb4414b4bde03a956b50eddb46ad01dfc03
Pamela Abreu <43766342+pamelaabreu@users.noreply.github.com>
2019-06-13 16:23:23 -0400

    Airbnb Calendar (#73)
    * Installed react-dates and updated package versions
    
    * Created style sheet to override React date class styles
    
    * Imported necessary code to activate React dates library
    
    * Added transitions to certain class styles
    
    * Implemented React Dates calendar library
    
    * Created class styles for the createTripForm input
    
    * Removed unused class styles and tweaked the publicHomeBanner
    
    * Modified the JSX semantics for the CreateTrip
    
    * Made new class styles to target the createTrip button
    
    * Changed the showModal to disable props for createTripForm
    
    * Removed JSX from PublicHome
    
    * Added bundle gifs
    
    * Changed the JSX semantics and classnames for publicHome
    
    * Removed unused class styles and made some new ones
    
    * Added border-radius style to createTripform-input class style

--- src/components/PublicHome/PublicHome.js
+++ src/components/PublicHome/PublicHome.js
@@ -1,57 +1,35 @@
 import React from "react";
 import "./PublicHome.css";
 import BundleLogo from "../../assets/images/logo/bundle_logo.svg";
+// import BundleLoad from "../../assets/images/background_images/bundle_load.gif";
 import LoadingScreen from "../LoadingScreen/LoadingScreen";
 
 const PublicHome = props => {
   const { create_trip_form, loading } = props;
 
   return (
-    <>
+    <div className="publicHomeBanner m-0 p-0 min-vh-100 min-vw-100 d-flex justify-content-center align-items-center">
+      {/* <div className="bg-bundleBlue m-0 p-0 min-vh-100 min-vw-100 d-flex justify-content-center align-items-center">   */}
       {!loading ? null : <LoadingScreen />}
-      <div className="publicHomeBanner container-fluid mb-5 min-vh-100">
-        <div className="row p-5 h-75">
-          <div className="col-sm m-5 pt-0 px-5 pb-2">
-            <img src={BundleLogo} width="150" height="150" alt="Bundle" />
-            <h2 className="c-white mali700 mt-5 h1">
-              Worry less, travel more!
-            </h2>
-          </div>
 
-          <div className="col-sm m-5 p-5 bg-huate80 b-radius9 pb-5">
-            <h2 className="c-bundleBlue mali700 mb-5 h1">Let's get packing!</h2>
-            {create_trip_form}
-          </div>
+      <div className="row p-5 m-0 publicHome-width">
+        <div className="col-sm-6 col-md-6 col-lg-6 text-center">
+          <img
+            src={BundleLogo}
+            width="70%"
+            height="70%"
+            className=""
+            alt="Bundle"
+          />
+          {/* <img src={BundleLoad} width="100%" height="100%" className="img-thumbnail border-0 p-0 bg-transparent" alt="Bundle" /> */}
+        </div>
+
+        <div className="col-sm-6 col-md-6 col-lg-6 p-5 bg-denimBlue b-radius9 ds-lightGrey createTripForm-mb">
+          <h2 className="c-huate mali700 mb-5 display-3">Let's get packing!</h2>
+          {create_trip_form}
         </div>
       </div>
-
-      <div className="container my-5">
-        <div className="row">
-          <div className="col-sm mr-2">
-            <div className="publicHomeWhatPhotoBox" />
-          </div>
-          <div className="col-sm">
-            <h2 className="mali700 h1 c-bundleBlue">What's Bundle?</h2>
-            <p className="mali400 h5 c-smokeGrey">
-              We provide a trip-management hub for inexperienced travelers to
-              keep track of all their necessities. They’ll have a smoother and
-              more enjoyable experience preparing for it because they can
-              address all their travel considerations from one place. Bundle
-              creates suggested packing checklists and help complete them with
-              in-app planning until day of departure.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="jumbotron h-50 mb-0 bundleHomeGetPackingBox">
-        <div className="container">
-          <h1 className="display-4 c-white baloo text-center">
-            What are you waiting for? Get Packing!
-          </h1>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
--- src/containers/CreateTripForm/CreateTripForm.js
+++ src/containers/CreateTripForm/CreateTripForm.js
@@ -1,83 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 import "./CreateTripForm.css";
+import { DateRangePicker } from "react-dates";
 
 export default props => {
-  let today = moment().format("YYYY-MM-DD");
-  let tomorrow = moment()
-    .add(1, "day")
-    .format("YYYY-MM-DD");
+  const { startDateHandler, endDateHandler } = props;
+
+  const [focusedInput, setFocusInput] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  useEffect(() => {
+    startDateHandler(moment(startDate).format("YYYY-MM-DD"));
+  }, [startDate, startDateHandler]);
+
+  useEffect(() => {
+    endDateHandler(moment(endDate).format("YYYY-MM-DD"));
+  }, [endDate, endDateHandler]);
 
   const disabledButtonDisplay = props.disabled
-    ? "bg-white c-smokeGrey border-0 p-3 h4 baloo b-radius9"
-    : "bundleBlueButton border-0 p-3 createTripform-button h4 baloo";
+    ? "bg-smokeGrey c-white border-0 p-3 h2 baloo b-radius9 createTripform-button-disabled"
+    : "bundleBlueButton border-0 p-3 createTripform-button h2 baloo";
+
+  const onDateChange = (startDate, endDate) => {
+    setEndDate(endDate);
+    setStartDate(startDate);
+  };
 
   return (
     <>
-      <div className="create-trip-form">
-        <form className="" onSubmit={props.createTripHandler}>
-          <div className="form-group">
-            <label htmlFor="destination" className="c-bundleBlue h4">
-              Enter your destination:
-            </label>
+      <form className="" onSubmit={props.createTripHandler}>
+        <div className="form-group">
+          <label htmlFor="destination" className="c-huate h2">
+            Enter your destination:
+          </label>
+
+          <div className="d-flex">
+            <div className="bg-white d-flex justify-content-center align-items-center">
+              <i className="fas fa-globe-americas createTrip-icon" />
+            </div>
+
             <input
-              className="form-control c-denimBlue bundleBlue-border-bottom-3 h4"
+              className="createTripform-input c-smokeBlack mali400 border-0 p-3 w-100"
               type="text"
               name="destination"
               placeholder="City, Country"
               aria-label="Destination by City, Country"
+              aria-describedby="basic-addon1"
               onChange={props.destinationHandler}
               value={props.destination}
               required
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="destination" className="c-bundleBlue h4">
-              Enter departure date:
-            </label>
-            <input
-              className="form-control c-denimBlue bundleBlue-border-bottom-3 h4"
-              type="date"
-              id="departure"
-              name="departure"
-              min={today}
-              required
-              aria-label="Departure Date"
-              pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
-              onChange={props.departureDateHandler}
-              value={props.departureDate}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="destination" className="c-bundleBlue h4">
-              Enter return date:
-            </label>
-            <input
-              className="form-control c-denimBlue bundleBlue-border-bottom-3 h4"
-              type="date"
-              id="return"
-              name="return"
-              min={tomorrow}
-              aria-label="Return Date"
-              required
-              pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
-              onChange={props.returnDateHandler}
-              value={props.returnDate}
-            />
-          </div>
-          <div className="d-flex justify-content-center p-2">
-            <button
-              type="submit button"
-              className={disabledButtonDisplay}
-              data-toggle="modal"
-              data-target="#modalScrollable"
-              disabled={props.disabled}
-            >
-              Next
-            </button>
-          </div>
-        </form>
-      </div>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="date" className="c-huate h2">
+            Enter your dates:
+          </label>
+          <DateRangePicker
+            noBorder={true}
+            block={true}
+            numberOfMonths={1}
+            daySize={26}
+            screenReaderInputMessage={"Enter your depature and return date."}
+            showDefaultInputIcon={true}
+            hideKeyboardShortcutsPanel={true}
+            startDate={startDate}
+            startDateId="startDate"
+            endDateId="endDate"
+            endDate={endDate}
+            onDatesChange={({ startDate, endDate }) =>
+              onDateChange(startDate, endDate)
+            }
+            focusedInput={focusedInput}
+            onFocusChange={focusedInput => setFocusInput(focusedInput)}
+          />
+        </div>
+        <div className="d-flex justify-content-center p-2">
+          <button
+            type="submit button"
+            className={disabledButtonDisplay}
+            data-toggle="modal"
+            data-target="#modalScrollable"
+            disabled={props.disabled}
+          >
+            Next
+          </button>
+        </div>
+      </form>
     </>
   );
 };
