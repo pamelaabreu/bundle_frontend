81b642f941a34f5a09b79c58ae7b18497d2c4941
Pamela Abreu <pamelaabreu@pursuit.com>
2019-06-13 22:18:34 -0400

    Updated JSX semantics and classnames

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -3,14 +3,14 @@ import "./LoadingScreen.css";
 
 const LoadingScreen = props => {
   return (
-    <div className="fullScreen">
-      <div className="fullScreenInnerBlock">
-        <img
-          src="https://media.giphy.com/media/d7nbVpR6n9rwiGUNBf/giphy.gif"
-          className="spinningLoading hueRotate pb-5"
-          alt="Loading..."
-        />
-        <h1 className="colorWhite">Loading...</h1>
+    <div className="loading-fullscreen">
+      <div className="loading-InnerBlock">
+        <div className="loadingScreen-container">
+          <div className="loadBundle-img" />
+          <h1 className="c-huate mali700 loadBundle-text display-2">
+            loading...
+          </h1>
+        </div>
       </div>
     </div>
   );
