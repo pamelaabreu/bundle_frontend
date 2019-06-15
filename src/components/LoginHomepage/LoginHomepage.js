f3c1cb0482b833153e59121fd82334c10d7e5e62
Alexander Onate <aionate0812@gmail.com>
2019-06-15 01:18:08 -0400

    Logged home styling (#88)
    * ability to add itinerary
    
    * trips page styling for <600px and >992px screens
    
    * increased size of trip page background color
    
    * logged home styling

--- src/components/LoginHomepage/LoginHomepage.js
+++ src/components/LoginHomepage/LoginHomepage.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import "./LoginHomepage.css";
+
 import LoginHomepageTripCard from "../LoginHomepageTripCard/LoginHomepageTripCard";
 import baseURL from "../../services/backendUrlConnect";
 const tripEndpointBase = "/login_homepage/";
@@ -33,33 +35,58 @@ class LoginHomepage extends Component {
     const { upcoming_trips, past_trips } = trips;
     const { create_trip_form: createTripForm, user } = this.props;
     return (
-      <div className="container">
-        <div>
-          <h1>Upcoming Trips</h1>
-          <div className="col-12 row" style={{ overflowY: "scroll" }}>
+      <div className="container-fluid login-home-container">
+        <div className="container">
+          <h1 className="login-home-trips-header">Upcoming Trips</h1>
+          <div
+            className="col-12 row mt-4 flex-nowrap"
+            style={{ overflowX: "scroll" }}
+          >
             {upcoming_trips &&
               upcoming_trips.map((e, i) => (
                 <LoginHomepageTripCard key={i} trip={e} />
               ))}
           </div>
         </div>
-
-        <div>
-          <h1>Past Trips</h1>
-          <div className="col-12 row" style={{ overflowY: "scroll" }}>
+        <div className="lg-screen-space" />
+        <div className="container mt-5">
+          <h1 className="login-home-trips-header">Past Trips</h1>
+          <div
+            className="col-12 row mt-4 flex-nowrap"
+            style={{ overflowX: "scroll" }}
+          >
             {past_trips &&
               past_trips.map((e, i) => (
                 <LoginHomepageTripCard key={i} trip={e} index={i} />
               ))}
           </div>
         </div>
-        <div className="row justify-content-center">
-          <div>
-            <div onClick={this.createNewTripClickHandler}>
-              {createNewTrip ? <h4>Cancel</h4> : <h4>Create a trip</h4>}
-            </div>
-            {createNewTrip ? createTripForm : null}
+        <div className="mt-5">
+          <div className="row col-12 justify-content-center">
+            {createNewTrip ? (
+              <button
+                onClick={this.createNewTripClickHandler}
+                className="btn btn-danger btn-lg"
+              >
+                Cancel
+              </button>
+            ) : (
+              <button
+                className="btn btn-primary btn-lg"
+                onClick={this.createNewTripClickHandler}
+              >
+                Create a trip
+              </button>
+            )}
           </div>
+
+          {createNewTrip ? (
+            <div className="row justify-content-center m-0 mt-4">
+              <div className="col-sm-6 col-md-6 col-lg-6 p-5 bg-denimBlue b-radius9 ds-lightGrey createTripForm-mb">
+                {createTripForm}
+              </div>
+            </div>
+          ) : null}
         </div>
       </div>
     );
